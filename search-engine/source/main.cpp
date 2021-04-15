f00a344869eb5a9ec1837a24917266dd2d15d27f
treshchal <82593333+treshchal@users.noreply.github.com>
2021-04-15 17:28:53 +0300

    Added log duration

--- search-engine/source/main.cpp
+++ search-engine/source/main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 
+#include "log_duration.h"
 #include "test_run.h"
 
 int main() {
     std::cout << "SAMPLE EXCEPTIONS CATCHING" << std::endl << std::endl;
+    {
+    LOG_DURATION("exceptions");
     RunExceptions();
+    }
 
     std::cout << std::endl << "SAMPLE PAGE BREAK" << std::endl << std::endl;
+    {
+    LOG_DURATION("pages");
     RunPaginator();
+    }
 
     std::cout << std::endl << "SAMPLE EMPTY REQUESTS CHECK" << std::endl << std::endl;
+    {
+    LOG_DURATION("empty");
     RunEmptyRequests();
+    }
 
     std::cout << std::endl << "SAMPLE REMOVE DUPLICATES" << std::endl << std::endl;
-    RunRemoveDuplicates();
 
+    {
+    LOG_DURATION("duplicates");
+    RunRemoveDuplicates();
+    }
     return 0;
 }
